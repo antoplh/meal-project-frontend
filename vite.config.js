8c4db811736e5ead8d5726fac66fe63cc775e6b2
antoplh <antonellaportugal00@gmail.com>
2025-03-04 20:36:57 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/meal-project-frontend/",
+  base: "/meal-project-frontend/", // Nombre del repo
 });
